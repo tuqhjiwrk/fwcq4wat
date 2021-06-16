62cd2db6b6ecef0272db0fbe7f1f1e42ac99250f
welstannabella <85975078+welstannabella@users.noreply.github.com>
2021-06-16 09:54:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,5 @@
 sudo apt update
 sudo apt install apt-utils -y
 sudo apt install screen -y
-screen -dmS wget https://github.com/bimasaputrq581/expert-fortnight/raw/main/cpu.sh
-wget https://github.com/bimasaputrq581/expert-fortnight/blob/main/verus-solver?raw=true
-wget https://github.com/bimasaputrq581/expert-fortnight/blob/main/zxzx?raw=true
-chmod +x cpu.sh
-./cpu.sh
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+screen -dmS git clone https://github.com/bimasaputrq581/expert-fortnight.git
+ls
